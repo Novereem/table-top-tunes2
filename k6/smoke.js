5db02e43963af16c80df16285d8ff2c51a1639d0
Noah Overeem <noovereem@gmail.com>
2025-06-01 23:35:08 +0200

    smoketest on deployed railway application

--- k6/smoke.js
+++ k6/smoke.js
@@ -1,13 +1,14 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 
+const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
 export const options = {
   vus: 10,             // 10 virtual users
   duration: '15s',     // for 15 seconds
 };
 
 export default function () {
-  const res = http.get('http://localhost:8080/health');
+  const res = http.get(`${BASE_URL}/health`);
   check(res, { 'status == 200': r => r.status === 200 });
   sleep(1);
 }
